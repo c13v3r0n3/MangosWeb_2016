4fbf42f6da062fd3c394b627682a6daa65c76292
c13v3r0n3 <clever.one@gmail.com>
2016-10-28 16:23:01 -0500

    3.0.5
    Update depreciated mysql to mysqli

--- core/class.database.php
+++ core/class.database.php
@@ -15,39 +15,39 @@ class Database
         'time'  => 0,
         'count' => 0,
     );
-    private $mysql;
+    private $mysqli;
 
 /************************************************************
-* Creates the connection to the mysql database, selects the posted DB
+* Creates the connection to the mysqli database, selects the posted DB
 * Returns 0 if unable to connect to the database
 * Returns 2 if the Database does not exist
 * Returns TRUE on success
 */
 
-    public function __construct($db_host, $db_port, $db_user, $db_pass, $db_name)
+    public function __construct($db_host, $db_user, $db_pass, $db_name, $db_port)
     {
-        $this->mysql = @mysql_connect($db_host.":".$db_port, $db_user, $db_pass, true);
-        $this->selected_database = @mysql_select_db($db_name, $this->mysql);
+        $this->mysqli = @mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
+        $this->selected_database = @mysqli_select_db($this->mysqli, $db_name);
 		return TRUE;
     }
 
 //	************************************************************
-// Closes the mysql DB connection
+// Closes the mysqli DB connection
 
     public function __destruct()
     {
-        @mysql_close($this->mysql) or die(mysql_error());
+        @mysqli_close($this->mysqli) or die(mysqli_error($this->mysqli));
     }
-	
+
 /************************************************************
-* Checks the connection to the mysql database, selects the posted DB
+* Checks the connection to the mysqli database, selects the posted DB
 * Returns 0 if unable to connect to the database
 * Returns 2 if the Database does not exist
 * Returns TRUE on success
-*/	
+*/
 	public function status()
 	{
-		if(!$this->mysql)
+		if(!$this->mysqli)
 		{
 			return 0;
 		}
@@ -63,7 +63,7 @@ class Database
 
     public function query($query)
     {
-        $sql = mysql_query($query,$this->mysql) or die("Couldnt Run Query: ".$query."<br />Error: ".mysql_error($this->mysql)."");
+        $sql = mysqli_query($this->mysqli, $query) or die("Couldnt Run Query: ".$query."<br />Error: ".mysqli_error($this->mysqli)."");
 		$this->_statistics['count']++;
 		return TRUE;
     }
@@ -73,16 +73,16 @@ class Database
 
     public function select($query)
     {
-        $sql = mysql_query($query,$this->mysql) or die("Couldnt Run Query: ".$query."<br />Error: ".mysql_error($this->mysql)."");
+        $sql = mysqli_query($this->mysqli, $query) or die("Couldnt Run Query: ".$query."<br />Error: ".mysqli_error($this->mysqli)."");
 		$this->_statistics['count']++;
 		$i = 1;
-		if(mysql_num_rows($sql) == 0)
+		if(mysqli_num_rows($sql) == 0)
 		{
 			$result = FALSE;
 		}
 		else
 		{
-			while($row = mysql_fetch_assoc($sql))
+			while($row = mysqli_fetch_assoc($sql))
 			{
 				foreach($row as $colname => $value)
 				{
@@ -94,87 +94,87 @@ class Database
 		return $result;
     }
 
-//	************************************************************	
+//	************************************************************
 // selectRow is perfect for getting 1 row of data. Technically can be used for multiple rows,
 // though select function is better for more then 1 row
 
 	public function selectRow($query)
     {
-        $sql = mysql_query($query,$this->mysql) or die("Couldnt Run Query: ".$query."<br />Error: ".mysql_error($this->mysql)."");
+        $sql = mysqli_query($this->mysqli, $query) or die("Couldnt Run Query: ".$query."<br />Error: ".mysqli_error($this->mysqli)."");
 		$this->_statistics['count']++;
-		if(mysql_num_rows($sql) == 0)
+		if(mysqli_num_rows($sql) == 0)
 		{
 			return FALSE;
 		}
 		else
 		{
-			$row = mysql_fetch_array($sql);
+			$row = mysqli_fetch_array($sql);
 			return $row;
 		}
     }
-	
+
 //	************************************************************
 // selectCell returns 1 cell of data, Not recomended unless you want data from a specific cell in a table
 
 	public function selectCell($query)
     {
-        $sql = mysql_query($query,$this->mysql) or die("Couldnt Run Query: ".$query."<br />Error: ".mysql_error($this->mysql)."");
+        $sql = mysqli_query($this->mysqli, $query) or die("Couldnt Run Query: ".$query."<br />Error: ".mysqli_error($this->mysqli)."");
 		$this->_statistics['count']++;
-		if(mysql_num_rows($sql) == 0)
+		if(mysqli_num_rows($sql) == 0)
 		{
 			return FALSE;
 		}
 		else
 		{
-			$row = mysql_fetch_array($sql);
+			$row = mysqli_fetch_array($sql);
 			return $row['0'];
 		}
     }
 
-//	************************************************************	
+	//	************************************************************
 // count is a perfect function for counting the num of rows, or results in a table
 // returns the direct count, for ex: 5
 
 	public function count($query)
     {
-        $sql = mysql_query($query, $this->mysql) or die("Couldnt Run Query: ".$query."<br />Error: ".mysql_error($this->mysql)."");
+        $sql = mysqli_query($this->mysqli, $query) or die("Couldnt Run Query: ".$query."<br />Error: ".mysqli_error($this->mysqli)."");
 		$this->_statistics['count']++;
-		return mysql_result($sql, 0);
+		return mysqli_result($sql, 0);
     }
 
-//	************************************************************	
+//	************************************************************
 // Run a sql file function. Not written by me.
 // $file is the path location to the sql file
 
 	function runSQL($file)
 	{
 		$handle = @fopen($file, "r");
-		if ($handle) 
+		if ($handle)
 		{
-			while(!feof($handle)) 
+			while(!feof($handle))
 			{
 				$sql_line[] = fgets($handle);
 			}
 			fclose($handle);
 		}
-		else 
+		else
 		{
 			return FALSE;
 		}
-		foreach ($sql_line as $key => $query) 
+		foreach ($sql_line as $key => $query)
 		{
-			if (trim($query) == "" || strpos ($query, "--") === 0 || strpos ($query, "#") === 0) 
+			if (trim($query) == "" || strpos ($query, "--") === 0 || strpos ($query, "#") === 0)
 			{
 				unset($sql_line[$key]);
 			}
 		}
 		unset($key, $query);
 
-		foreach ($sql_line as $key => $query) 
+		foreach ($sql_line as $key => $query)
 		{
 			$query = rtrim($query);
 			$compare = rtrim($query, ";");
-			if ($compare != $query) 
+			if ($compare != $query)
 			{
 				$sql_line[$key] = $compare . "|br3ak|";
 			}
@@ -183,12 +183,12 @@ class Database
 
 		$sql_lines = implode($sql_line);
 		$sql_line = explode("|br3ak|", $sql_lines);
-		
+
 		foreach($sql_line as $query)
 		{
 			if($query)
 			{
-				mysql_query($query, $this->mysql) or die("Couldnt Run Query: ".$query."<br />Error: ".mysql_error($this->mysql)."");
+				mysqli_query($query, $this->mysqli) or die("Couldnt Run Query: ".$query."<br />Error: ".mysqli_error($this->mysqli)."");
 			}
 		}
 		return TRUE;
